ebb81a6e7809780daba36bfcbd7d6df32cefbd0f
Xuyuanjia2014 <xuyuanjia2017@otcaix.iscas.ac.cn>
2021-01-26 04:43:48 -0600

    bad

---
+++ tensorflow-analyzer/init-model.sh
@@ -0,0 +1,7 @@
+NUM_GPUS=1
+DATA_DIR=/root/github/tensorflow-dataset
+MODEL_DIR=/root/github/tensorflow-model
+export PYTHONPATH=$PYTHONPATH:/root/github/Talos/models
+
+# Mnist
+# python3 mnist_main.py   --model_dir=$MODEL_DIR   --data_dir=$DATA_DIR   --train_epochs=10   --distribution_strategy=one_device   --num_gpus=$NUM_GPUS   --download
