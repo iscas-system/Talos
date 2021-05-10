ae5185f33ffe291c48168e593530bf74db441fcc
Xuyuanjia2014 <794774196@qq.com>
2021-05-11 00:09:19 +0800

    small errors

--- tvm-analyzer/operator/update_schedule.py
+++ tvm-analyzer/operator/update_schedule.py
@@ -4,12 +4,12 @@ from tvm import te
 import numpy as np
 
 def update_by_parallel(variables, name):
-    if name = "myadd":
+    if name == "myadd":
         variables[2].parallel(C.op.axis[0])
     return variables
 
-def update_by_vectorization_on_cpu(variables, name, factor)
-    if name = "myadd":
+def update_by_vectorization_on_cpu(variables, name, factor):
+    if name == "myadd":
         variables[2].parallel(C.op.axis[0])
         outer, inner = variables[2].split(C.op.axis[0], factor=factor)
         variables[2].parallel(outer)
