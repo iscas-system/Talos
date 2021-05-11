60f1d2ea459bd4496e9c39a331a741206f5f7a51
Xuyuanjia2014 <794774196@qq.com>
2021-05-12 00:15:03 +0800

    gpu operator optimization

--- tvm-analyzer/operator/_main.py
+++ tvm-analyzer/operator/_main.py
@@ -5,14 +5,14 @@ import numpy as np
 from describe_computation import create_simple_sum
 from compile_operator import compile_execute_raw_operator
 from execute_operator import evaluate_addition
-from update_schedule import update_by_parallel, update_by_vectorization_on_cpu
+from update_schedule import update_by_parallel, update_by_vectorization_on_cpu, update_by_parallel_gpu
 
 def create_compile_execute_operator(create_fun, tgt, optimize_type="naive", name="myadd", update_schedule_fun=None, factor= 4):
-    # create schedulue an operator on the target:
+    # create schedulue an operator without device:
     schedule, variables = create_fun()
     if update_schedule_fun != None:
         schedule = update_schedule_fun(schedule, variables, name, factor)
-    # compile the operator
+    # compile the operator on target:
     operator = compile_execute_raw_operator(schedule, variables, tgt, name=name)
     # execute (compare, evaluate) operator by evaluate execution time:
     evaluate_addition(operator, tgt, optimize_type, name, variables)
@@ -23,4 +23,6 @@ create_compile_execute_operator(create_simple_sum, tvm.target.Target(target="llv
 create_compile_execute_operator(create_simple_sum, tvm.target.Target(target="llvm", host="llvm"), optimize_type="parallel", name="myadd_parallel", update_schedule_fun = update_by_parallel)
 
 create_compile_execute_operator(create_simple_sum, tvm.target.Target(target="llvm", host="llvm"), optimize_type="vectorization", name="myadd_vectorization", update_schedule_fun = update_by_vectorization_on_cpu, factor = 20)
+
+create_compile_execute_operator(create_simple_sum, tvm.target.Target(target="cuda", host="llvm"), optimize_type="vectorization", name="myadd_gpu_vectorliazaiton", update_schedule_fun = update_by_parallel_gpu, factor = 20)
 
--- tvm-analyzer/operator/update_schedule.py
+++ tvm-analyzer/operator/update_schedule.py
@@ -14,3 +14,10 @@ def update_by_vectorization_on_cpu(schedule, variables, name, factor):
         schedule[variables[2]].parallel(outer)
         schedule[variables[2]].vectorize(inner)
     return schedule
+
+def update_by_parallel_gpu(schedule, variables, name, factor):
+    if "myadd" in name:
+        bx, tx = schedule[variables[2]].split(variables[2].op.axis[0], factor=factor)
+        schedule[variables[2]].bind(bx, te.thread_axis("blockIdx.x"))
+        schedule[variables[2]].bind(tx, te.thread_axis("threadIdx.x"))
+    return schedule
