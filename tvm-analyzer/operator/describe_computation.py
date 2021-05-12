d2b933e54e108be024d751795882c8cc48f4a560
Xuyuanjia2014 <794774196@qq.com>
2021-05-12 22:59:02 +0800

    matmul raw

--- tvm-analyzer/operator/describe_computation.py
+++ tvm-analyzer/operator/describe_computation.py
@@ -11,3 +11,17 @@ def create_simple_sum():
     C = te.compute(A.shape, lambda i: A[i] + B[i], name="C")
     s = te.create_schedule(C.op)
     return s, [A,B,C]
+
+def create_matmul_basic():
+    a = te.placeholder((n, l), name="a", dtype=dtype)
+    B = te.placeholder((L, M), name="B", dtype=dtype)
+    k = te.reduce_axis((0, L), name="k")
+    C = te.compute((N, M), lambda i, j: te.sum(A[i, k] * B[k, j], axis=k), name="C")
+    s = te.create_schedule(C.op)
+    # schedule
+    y, x = s[C].op.axis
+    k = s[C].op.reduce_axis[0]
+    yo, yi = s[C].split(y, 8)
+    xo, xi = s[C].split(x, 8)
+    s[C].reorder(yo, xo, k, yi, xi)
+    return s, [A, B, C]
