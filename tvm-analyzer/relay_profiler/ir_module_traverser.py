78a5a0624997e7717201f94d691c846df3bc5394
Xuyuanjia2014 <794774196@qq.com>
2021-05-19 14:26:18 +0800

    verify args and params stack

--- tvm-analyzer/relay_profiler/ir_module_traverser.py
+++ tvm-analyzer/relay_profiler/ir_module_traverser.py
@@ -10,16 +10,15 @@ str = ""
 
 def profile_relay_operator(ir_module, ir_params,i):
     print("current entrance function length:", len(ir_module.functions.items()))
+    print(ir_module)
     entrance_tuple = ir_module.functions.items()[0]
     global_var = entrance_tuple[0]
     main_function = entrance_tuple[1]
-    print(type(main_function.params))
     print_tree(i,main_function.body.op.name)
-    for each_param in main_function.params:
-        print(type(each_param))
-    print("")
-    for each_arg in main_function.body.args:
-        print(type(each_arg))
+    # for each_param in main_function.params:
+    #     print(type(each_param))
+    # print(type(main_function))
+    recursive_traverse_op(main_function.body.op.name, main_function.body.attrs, main_function.body.args)
 
     print("IR graph")
     print(str)
@@ -30,3 +29,11 @@ def print_tree(i, op_name):
         str+=" "
     str+=op_name
     str+="\n"
+
+def recursive_traverse_op(name, attrs, args):
+    print(name,attrs)
+    for each_arg in args:
+        if isinstance(each_arg, tvm.relay.expr.Call):
+            recursive_traverse_op(each_arg.op, each_arg.attrs, each_arg.args)
+        if isinstance(each_arg,tvm.relay.expr.Var):
+            print("param Node:",each_arg)
