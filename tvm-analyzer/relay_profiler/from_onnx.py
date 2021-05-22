1d77269db6079f1514ba6416171f21c607c75232
Xuyuanjia2014 <794774196@qq.com>
2021-05-22 23:55:44 +0800

    bad

--- tvm-analyzer/relay_profiler/from_onnx.py
+++ tvm-analyzer/relay_profiler/from_onnx.py
@@ -1,37 +1,4 @@
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
-"""
-Compile ONNX Models
-===================
-**Author**: `Joshua Z. Zhang <https://zhreshold.github.io/>`_
 
-This article is an introductory tutorial to deploy ONNX models with Relay.
-
-For us to begin with, ONNX package must be installed.
-
-A quick solution is to install protobuf compiler, and
-
-.. code-block:: bash
-
-    pip install onnx --user
-
-or please refer to offical site.
-https://github.com/onnx/onnx
-"""
 import onnx
 import numpy as np
 import tvm
@@ -43,12 +10,6 @@ from tvm.relay.testing import check_grad, run_infer_type
 from tvm.relay.transform import gradient
 from ir_module_traverser import construct_op_graph
 
-######################################################################
-# Load pretrained ONNX model
-# ---------------------------------------------
-# The example super resolution model used here is exactly the same model in onnx tutorial
-# http://pytorch.org/tutorials/advanced/super_resolution_with_caffe2.html
-# we skip the pytorch model construction part, and download the saved onnx model
 model_url = "".join(
     [
         "https://gist.github.com/zhreshold/",
@@ -58,17 +19,8 @@ model_url = "".join(
     ]
 )
 model_path = download_testdata(model_url, "super_resolution.onnx", module="onnx")
-# now you have super_resolution.onnx on disk
 onnx_model = onnx.load(model_path)
 
-######################################################################
-# Load a test image
-# ---------------------------------------------
-# A single cat dominates the examples! This model takes a single input image of size
-# 224x224 and outputs a scaled image that is 3x greater than the input along each
-# axis, a 672x672 image. Re-scale the cat image to fit this input shape then
-# convert to `YCbCr`. The super resolution model will then be applied to the
-# luminance (`Y`) channel.
 from PIL import Image
 
 img_url = "https://github.com/dmlc/mxnet.js/blob/main/data/cat.png?raw=true"
@@ -78,17 +30,6 @@ img_ycbcr = img.convert("YCbCr")  # convert to YCbCr
 img_y, img_cb, img_cr = img_ycbcr.split()
 x = np.array(img_y)[np.newaxis, np.newaxis, :, :]
 
-######################################################################
-# Compile the model with relay
-# ---------------------------------------------
-# Typically ONNX models mix model input values with parameter values, with
-# the input having the name `1`. This model dependent, and you should check
-# with the documentation for your model to determine the full input and
-# parameter name space.
-#
-# Passing in the shape dictionary to the `relay.frontend.from_onnx` method
-# tells relay which ONNX parameters are inputs, and which are parameters, and
-# provides a static definition of the input size.
 target = "llvm"
 
 input_name = "1"
